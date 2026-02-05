c055316d19b89f2b6b144acd25bfb3e93db847e7
riofach <fachrio.raditya@gmail.com>
2026-02-05 13:46:57 +0700

    feat(01-02): create Header component structure

---
+++ src/components/layout/Header.tsx
@@ -0,0 +1,14 @@
+"use client"
+
+import { motion, useScroll, useMotionValueEvent, useSpring } from "motion/react"
+
+export function Header() {
+  return (
+    <header>
+      {/* Content will go here */}
+      <nav>
+        {/* Nav items */}
+      </nav>
+    </header>
+  )
+}
