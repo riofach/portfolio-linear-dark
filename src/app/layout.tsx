875e5c0c0a1da11e744f6d2cba09de9c7f79e1f7
riofach <fachrio.raditya@gmail.com>
2026-02-05 18:40:22 +0700

    feat(02-05): add footer to global layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import { ReactLenis } from "lenis/react";
 import "./globals.css";
 import { Header } from "@/components/layout/Header";
+import { Footer } from "@/components/layout/Footer";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -35,6 +36,7 @@ export default function RootLayout({
           <main>
             {children}
           </main>
+          <Footer />
         </ReactLenis>
       </body>
     </html>
