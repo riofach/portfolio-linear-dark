36c492268abf0dc3421b4586515dec14df00e6e7
riofach <fachrio.raditya@gmail.com>
2026-02-05 13:47:13 +0700

    feat(01-02): implement scroll progress bar

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -3,12 +3,24 @@
 import { motion, useScroll, useMotionValueEvent, useSpring } from "motion/react"
 
 export function Header() {
+  const { scrollYProgress } = useScroll()
+  const scaleX = useSpring(scrollYProgress, {
+    stiffness: 100,
+    damping: 30,
+    restDelta: 0.001
+  })
+
   return (
-    <header>
-      {/* Content will go here */}
-      <nav>
+    <header className="fixed top-0 left-0 w-full z-50">
+      <nav className="relative z-10">
         {/* Nav items */}
       </nav>
+      
+      {/* Scroll Progress Bar */}
+      <motion.div
+        className="absolute bottom-0 left-0 h-[1px] w-full bg-zinc-500 origin-left"
+        style={{ scaleX }}
+      />
     </header>
   )
 }
